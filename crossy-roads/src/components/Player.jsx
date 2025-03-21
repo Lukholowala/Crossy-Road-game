b147730df3d678c5900539cb64653e00e7c2b5f5
Lukholo <lukhowala@gmail.com>
2025-03-21 12:43:03 +0200

    Drawing the player component
    Used boxGeometry bcause the player is a simple box, meshLambertMaterial it responds to light

---
+++ crossy-roads/src/components/Player.jsx
@@ -0,0 +1,10 @@
+
+
+export function Player() {
+    return (
+        <mesh position={[0,0,10]}>
+            <boxGeometry args={[15, 15, 20]} />
+            <meshLambertMaterial color={0xffffff} flatShading/>
+        </mesh>
+    )
+}
